1d688a451ff6cb314637b9bef18bf956444b416f
NouraAhmed09 <132657585+NouraAhmed09@users.noreply.github.com>
2023-05-22 02:47:02 +0300

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -4,68 +4,88 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class HomePage extends JFrame {
     
-    private JLabel title;
-    private JButton StoreButton;
-   // private JButton AppointmentButton;
-   
-   private JPanel centerPanel;
-    
-    public HomePage() {
-        setTitle("NEEDS APP");
-        setSize(700, 350);
+   private JLabel label = new JLabel("Our services");
+
+    private ImageIcon imageIcon = new ImageIcon("physicaltherapy.jpg");
+    private  JLabel image = new JLabel(imageIcon);
+    private JButton bookButton = new JButton("           Booking an Appointment           ");
+    private JButton BuyButton = new JButton("           buying tools           ");
+
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu file = new JMenu("File");
+    private JMenu help = new JMenu("Help");
+    private JMenuItem close = new JMenuItem("Close");
+    private JMenuItem aboutUs = new JMenuItem("About Us");
+
+
+    public HomePage(){
+        setTitle("Needs App");
+        setSize(700, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setBackground(Color.decode("#7CACC2"));
+        setLayout(new FlowLayout(FlowLayout.CENTER,40,50));
+       getContentPane().setBackground(Color.decode("#f0f4f7"));
         setLocationRelativeTo(null);
-        
-         buildTitle();
-        buildHome();
-        
-       centerPanel = new JPanel();
-       centerPanel.setLayout(new GridLayout(4, 2, 0, 50));
-       centerPanel.setBackground(Color.decode("#7CACC2"));
-       
-      
-        
-        
-        add(title);
-        add(StoreButton);
-       // add(AppointmentButton);
-        add(centerPanel);
-        
+
+        label.setFont(new Font("Serif",Font.BOLD,30));
+        bookButton.setBackground(Color.decode("#4894fe"));
+        bookButton.setForeground(Color.white);
+        BuyButton.setBackground(Color.decode("#4894fe"));
+        BuyButton.setForeground(Color.white);
+
+        add(image);
+        add(label);
+        add(bookButton);
+        add(BuyButton);
+        file.add(close);
+        help.add(aboutUs);
+        menuBar.add(file);
+        menuBar.add(help);
+
+
+        setJMenuBar(menuBar);
+
         setVisible(true);
-        
-     
-        StoreButton.addActionListener(new StoreButton());
-        
-    }
 
-    private void buildTitle() {
-        
-        title = new JLabel("Select                      ");
-        title.setForeground(Color.decode("#D7D7CD"));
-        title.setHorizontalAlignment(JLabel.CENTER);
-        title.setFont(new Font("Serif", Font.BOLD, 23));
-        
-    }
-     private void buildHome() {
-        
-        StoreButton = new JButton("            Store            ");
-        StoreButton.setBackground(Color.decode("#5B5B5B"));
-        StoreButton.setForeground(Color.white);
-        
-    }
+        bookButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
 
-   
-    
-    private class StoreButton implements ActionListener {
+                setVisible(false);
 
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            //setVisible(false);
-            new Store();
-        }
+                Department dep = new Department();
+            }
+        });
+
+        BuyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                setVisible(false);
+
+                Store store = new Store();
+            }
+        });
+
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+
+        aboutUs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"In our application you can find All you Need for helping People with special needs");
+            }
+        });
+
     }
+      
 }
+
